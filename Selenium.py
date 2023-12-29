519b9c1e369a346144b4b5cd20c7c51ef8a1d1ea
EmilieD1 <136595775+EmilieD1@users.noreply.github.com>
2023-12-29 19:05:11 +0200

    Update Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -4,8 +4,6 @@ from selenium.webdriver.common.keys import Keys
 webdriver_path = '/path/to/your/webdriver'
 driver = webdriver.Chrome(executable_path=webdriver_path)
 driver.get("https://www.google.com")
-
-# Find the search input element using its name attribute value
 search_input = driver.find_element("name", "q")
 search_input.send_keys("Your search query")
 search_input.send_keys(Keys.RETURN)
